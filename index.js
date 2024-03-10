5761232d4af898acaca1a7757b37fb87a7e90473
swu <shuangwumetal@gmail.com>
2024-03-10 07:50:43 +0200

    syntax error and databse query fixed

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const app = require('./app') 
-const config = require('./utils/config')
+const app = require('./app');
+const config = require('./utils/config');
 const axios = require('axios');
-const Message = require('./models/message.model')
-const logger = require('./utils/logger')
-
+const Message = require('./models/message.model');
+const logger = require('./utils/logger');
 
 app.post('/new-message', async (req, res) => {
   const { message } = req.body;
@@ -13,42 +12,39 @@ app.post('/new-message', async (req, res) => {
   }
 
   const text = message.text.toLowerCase();
-	let responseText = '';
+  let responseText = '';
   if (text.includes("蛤")) {
-	responseText = " ∞ ∞ ∞ ∞ ∞ NAIVE!  ∞  ∞ ∞ ∞ ∞ ∞";
+    responseText = " ∞ ∞ ∞ ∞ ∞ NAIVE!  ∞  ∞ ∞ ∞ ∞ ∞";
   }
 
-  const mentioned = message.text.toLowerCase().includes(process.env.BOT_NAME)
+  const mentioned = message.text.toLowerCase().includes(process.env.BOT_NAME);
   if (mentioned) {
-	try {
-	  const totalDocuments = await Message.countDocuments();
-	  if (totalDocuments === 0) {
-		await sendMessage(message.chat.id, "There are no messages yet!");
-		res.end('ok');
-	  } else {
-		const randomId = Math.floor(Math.random() * totalDocuments); // Ensure non-zero value
-		const randomMessage = await Message.findById(randomId);
-		const randomMessageText = randomMessage ? randomMessage.content : 'No messages found';
-		await sendMessage(message.chat.id, randomMessageText);
-		res.end('ok');
-	  }
-	} catch (error) {
-	  console.error('Error:', error);
-	  res.end('Error');
-	}
+    try {
+      const randomMessage = await Message.aggregate([{ $sample: { size: 1 } }]);
+      const randomMessageText = randomMessage.length > 0 ? randomMessage[0].content : 'No messages found';
+      await sendMessage(message.chat.id, randomMessageText);
+      return res.end('ok');
+    } catch (error) {
+      logger.error('Error:', error);
+      return res.status(500).end('Error');
+    }
   }
+});
 
 const sendMessage = async (chatId, messageText) => {
-  await axios.post(
-    `https://api.telegram.org/bot${process.env.API_TOKEN}/sendMessage`,
-    {
-      chat_id: chatId,
-      text: messageText,
-    }
-  );
+  try {
+    await axios.post(
+      `https://api.telegram.org/bot${process.env.API_TOKEN}/sendMessage`,
+      {
+        chat_id: chatId,
+        text: messageText,
+      }
+    );
+  } catch (error) {
+    logger.error('Error sending message:', error);
+  }
 };
 
 app.listen(config.PORT, () => {
-	logger.info(`Server running on port ${config.PORT}`)
-  })
-})
+  logger.info(`Server running on port ${config.PORT}`);
+});
